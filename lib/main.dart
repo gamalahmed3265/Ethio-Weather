85a07afb5b4601e3c24e0c25fc95549dc403924f
gamal ahmed <gamalahmed3265@gmail.com>
2023-06-30 11:57:10 +0300

    Load configuration at runtime from a .env file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 
@@ -6,6 +7,8 @@ import 'src/app.dart';
 import 'src/providers/providers.dart';
 
 Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+
   WidgetsFlutterBinding.ensureInitialized();
 
   final hasInternetConnection = await InternetConnectionChecker().hasConnection;
