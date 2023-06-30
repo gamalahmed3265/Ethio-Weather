5992bf77f89267e30878e2bf41d966d9b56f15fe
gamal ahmed <gamalahmed3265@gmail.com>
2023-06-30 12:45:23 +0300

    Fixed the error

--- lib/data/data_source/reomte/onecall_api_service.dart
+++ lib/data/data_source/reomte/onecall_api_service.dart
@@ -3,7 +3,6 @@ import 'package:dio/dio.dart';
 import '../../../app/config.dart';
 import '../../../domain/model/lat_long.dart';
 import '../../../domain/model/open_weather_map.dart';
-
 import 'dio_client.dart';
 
 class OneCallApiService {
@@ -29,7 +28,7 @@ class OneCallApiService {
         return OpenWeatherMap.fromJson(response.data);
       }
       return Future.error('Failed to Open Weather data');
-    } on DioError catch (e) {
+    } on DioError {
       throw Exception("DioError: Failed to Open Weather data");
     }
   }
